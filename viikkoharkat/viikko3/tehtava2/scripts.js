df87356717c812a728292373b36689b8bd27ad7c
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2021-01-20 22:51:40 +0200

    week 3 task 2 wip

---
+++ viikkoharkat/viikko3/tehtava2/scripts.js
@@ -0,0 +1,9 @@
+function getInputValue(id){
+    return document.getElementById(id).value;
+}
+
+function calc(){
+    var input1 = getInputValue("input1");
+    var input2 = getInputValue("input2");
+    var input3 = getInputValue("input3");
+}
