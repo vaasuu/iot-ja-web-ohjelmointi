d56b5a5b8e567ffdb94c32665023528b89a0584c
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2021-01-26 13:57:59 +0200

    Added Prettier check

--- viikkoharkat/viikko4/weather-app/backend/src/index.js
+++ viikkoharkat/viikko4/weather-app/backend/src/index.js
@@ -1,6 +1,6 @@
-import 'babel-polyfill';
-import Koa from 'koa';
-import Router from 'koa-router';
+import "babel-polyfill";
+import Koa from "koa";
+import Router from "koa-router";
 
 // The port that this server will run on, defaults to 9000
 const port = process.env.PORT || 9000;
@@ -12,15 +12,15 @@ const app = new Koa();
 const test = new Router();
 
 // Define API path
-const apiPath = '/api/v1';
+const apiPath = "/api/v1";
 
 test.get(`${apiPath}/test`, async (ctx) => {
   // Tell the HTTP response that it contains JSON data encoded in UTF-8
-  ctx.type = 'application/json; charset=utf-8';
+  ctx.type = "application/json; charset=utf-8";
 
   // Put a static greeting message to the HTTP response body that will be sent to the browser
   // when a client navigates to http://hostname/api/v1/test
-  ctx.body = { greeting: 'Hello world! foobar' };
+  ctx.body = { greeting: "Hello world! foobar" };
 });
 
 app.use(test.routes());
